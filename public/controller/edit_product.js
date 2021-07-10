8df1da4a0a9c7c02f4ab613445c38f4364dd9c62
tnguyenthuyanh <tnguyenthuyanh@uco.edu>
2021-07-09 19:19:51 -0500

    sprint 1 done

--- public/controller/edit_product.js
+++ public/controller/edit_product.js
@@ -2,7 +2,7 @@ import * as FirebaseController from './firebase_controller.js'
 import * as Util from '../viewpage/util.js'
 import * as Constant from '../model/constant.js'
 import * as Element from '../viewpage/element.js'
-import { Product } from '../model/product.js';
+import { Product } from '../model/Product.js';
 
 let imageFile2Upload;
 
